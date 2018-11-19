1ca9393af5b44d8303ca809b2a19cec7772948fe
dvez <dylanvezendy@gmail.com>
2018-11-19 17:59:10 -0500

    gave movements with wasd

--- platformer/game.js
+++ platformer/game.js
@@ -1,23 +1,42 @@
+let pl, curs
+
 new Phaser.Game({
     width: 683,   // half of 1366
     height: 384, //half of 768
     physics: {
       default: 'arcade',
       arcade: {
-        gravity: {y: 300, x: 400 },
-        debug: true 
+        gravity: {y: 500},
+        debug: false 
       },
     },
     scene: {
         preload: function () {
-            this.load.image('bg', './assets/plat-bg.png')
-            this.load.image('fg', './assets/dylan5.png')
+            this.load.image('bg', './assets/city.sprite.png')
+            this.load.image('fg', './assets/lambo.png')
         },
         create() {
             this.add.image(0, 0, 'bg').setOrigin(0,0)
-            pl= this.physics.add.sprite(100, 90, 'fg')
+            pl = this.physics.add.sprite(100, 90, 'fg')
             pl.setCollideWorldBounds(true)
-            pl.setBounce(1) // 0 to 1
-        }
-    }
-})
+            pl.setBounce(.9) // 0 to 1
+            pl.setDragX(11)
+
+            curs = this.input.keyboard.createCursorKeys()
+      },
+
+        update() {
+
+            if (curs.left.isDown) {
+                pl.setVelocityX(-200)
+            }   else if (curs.right.isDown) {
+                pl.setVelocityX(200)
+            }
+         if (curs.space.isDown || curs.up.isDown) {
+             pl.setVelocityY(-600)
+         }
+
+       }
+   }
+}) 
+
